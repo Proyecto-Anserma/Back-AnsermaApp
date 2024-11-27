47f3d2e80e8820757a574a2355110d6fd3314eb0
miguelramos1053 <miguelramos1053@gmail.com>
2024-11-27 15:12:46 -0500

    Se hizo referencia con tabla genero

--- database/db_config.py
+++ database/db_config.py
@@ -1,3 +1,4 @@
+
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from databases import Database
@@ -15,6 +16,9 @@ engine_usuarios = create_async_engine(DATABASE_URL_USUARIOS, echo=True)
 async_session_usuarios = sessionmaker(bind=engine_usuarios, class_=AsyncSession, expire_on_commit=False)
 database_usuarios = Database(DATABASE_URL_USUARIOS)
 BaseUsuarios = declarative_base()
+
+from src.modulos.ciudadano.ciudadano_db_modelo import Ciudadano
+from src.modulos.genero.genero_db_modelo import Genero
 
 # Funciones para obtener sesiones de las bases de datos
 async def get_db_anserma():
--- src/modulos/ciudadano/ciudadano_rutas.py
+++ src/modulos/ciudadano/ciudadano_rutas.py
@@ -4,20 +4,19 @@ from sqlalchemy.future import select
 from geoalchemy2.elements import WKTElement
 from typing import List
 from shapely import wkt
-from .ciudadano_db_modelo import Ciudadano
-from .ciudadano_modelos import CiudadanoCreate, CiudadanoResponse, CiudadanoUpdate, CiudadanosFiltrar
+from .ciudadano_modelos import CiudadanoCreate, CiudadanosFiltrar, Ciudadano
 from database.db_config import get_db_anserma
 from .ciudadano_servicio import (
     get_ciudadanos,
     create_ciudadano,
-    update_ciudadano,
+    
     delete_ciudadano,
     filtrar_ciudadanos
 )
 
 router = APIRouter()
 
-@router.get("/ciudadanos/", response_model=List[CiudadanoResponse])
+@router.get("/ciudadanos/", response_model=List[Ciudadano])
 async def read_ciudadanos(db: AsyncSession = Depends(get_db_anserma)):
     try:
         ciudadanos = await get_ciudadanos(db)
@@ -29,7 +28,7 @@ async def read_ciudadanos(db: AsyncSession = Depends(get_db_anserma)):
         )
 
 
-@router.post("/ciudadanos/", response_model=CiudadanoResponse, status_code=status.HTTP_201_CREATED)
+@router.post("/ciudadanos/", response_model=Ciudadano, status_code=status.HTTP_201_CREATED)
 async def create_ciudadano_endpoint(ciudadano: CiudadanoCreate, db: AsyncSession = Depends(get_db_anserma)):
     try:
         ciudadano_dict = ciudadano.model_dump()
@@ -44,22 +43,22 @@ async def create_ciudadano_endpoint(ciudadano: CiudadanoCreate, db: AsyncSession
         )
         
         
-@router.put("/ciudadanos/{ciudadano_id}", response_model=CiudadanoResponse)
-async def update_ciudadano_endpoint(
-    ciudadano_id: str, 
-    ciudadano: CiudadanoUpdate, 
-    db: AsyncSession = Depends(get_db_anserma)
-):
-    try:
-        updated_ciudadano = await update_ciudadano(db, ciudadano_id, ciudadano)
-        if not updated_ciudadano:
-            raise HTTPException(status_code=404, detail="Ciudadano no encontrado")
-        return updated_ciudadano
-    except Exception as e:
-        raise HTTPException(
-            status_code=400,
-            detail=f"Error al actualizar ciudadano: {str(e)}"
-        )
+# @router.put("/ciudadanos/{ciudadano_id}", response_model=Ciudadano)
+# async def update_ciudadano_endpoint(
+#     ciudadano_id: str, 
+#     ciudadano: CiudadanoUpdate, 
+#     db: AsyncSession = Depends(get_db_anserma)
+# ):
+#     try:
+#         updated_ciudadano = await update_ciudadano(db, ciudadano_id, ciudadano)
+#         if not updated_ciudadano:
+#             raise HTTPException(status_code=404, detail="Ciudadano no encontrado")
+#         return updated_ciudadano
+#     except Exception as e:
+#         raise HTTPException(
+#             status_code=400,
+#             detail=f"Error al actualizar ciudadano: {str(e)}"
+#         )
         
 
 @router.delete("/ciudadanos/{ciudadano_id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -75,7 +74,7 @@ async def delete_ciudadano_endpoint(ciudadano_id: str, db: AsyncSession = Depend
             detail=f"Error al eliminar ciudadano: {str(e)}"
         )
     
-@router.post("/filtrar-ciudadanos/", response_model=List[CiudadanoResponse])
+@router.post("/filtrar-ciudadanos/", response_model=List[Ciudadano])
 async def filtrar_ciudadanos_endpoint(
     filtros: CiudadanosFiltrar = Body(...),
     db: AsyncSession = Depends(get_db_anserma)
